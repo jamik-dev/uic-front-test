863c859a2ea90da8f8e97091b33670b55beb0ec1
jamik-dev <jamik.oik.group@gmail.com>
2023-01-25 16:29:14 +0500

    adding line chart

---
+++ src/components/local/LineChart.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="flex bg-white p-6 rounded-lg w-full">
+    <apexchart type="line" class="w-full" height="400" :options="chartOptions" :series="series"></apexchart>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+const chartOptions = {
+  chart: {
+    type: 'line',
+    zoom: {
+      enabled: false
+    }
+  },
+  dataLabels: {
+    enabled: false
+  },
+  stroke: {
+    curve: 'smooth'
+  },
+  title: {
+    text: 'Homiylar va talabalar soni',
+    align: 'left',
+    style: {
+      fontSize: '26px'
+    }
+  },
+  xaxis: {
+    categories: ['Yanvar', 'Fevral', 'Mart', 'Aprel', 'May', 'Iyun', 'Iyul', 'Avgust', 'Sentabr', 'Oktabr', 'Noyabr', 'Dekabr'],
+  }
+}
+
+const series = [{
+  name: "Homiylar",
+  data: [5000, 20000, 5000, 30000, 10000, 40000, 20000, 45000, 25000, 40000, 20000, 35000],
+  color: '#4C6FFF'
+},
+{
+  name: "Talabalar",
+  data: [30000, 20000, 35000, 25000, 45000, 25000,45000, 10000, 45000, 20000, 40000, 20000],
+  color: '#FF92AE'
+}]
+</script>
+
+<style scoped>
+
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,19 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import 'vue-loaders/dist/vue-loaders.css';
-import VueLoaders from 'vue-loaders';
-import Notifications from '@kyvg/vue3-notification'
 import App from './App.vue'
 import router from './router'
+
+import 'vue-loaders/dist/vue-loaders.css';
+import Notifications from '@kyvg/vue3-notification'
+import VueLoaders from 'vue-loaders';
+import VueApexCharts from "vue3-apexcharts";
 
 import './assets/main.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(VueApexCharts)
 app.use(VueLoaders);
 app.use(Notifications)
 app.use(router)
